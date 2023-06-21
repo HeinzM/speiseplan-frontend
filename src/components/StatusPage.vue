40339d2755859359b5e56eeabf5ce75107cef88c
Matthias Möbius <mmoebi@gmx.de>
2023-06-21 20:56:30 +0200

    StatusPage.vue & StatusPageView.vue erstellt Statuspage mit bedingtem Pictureload StatusPageView.vue mit Übergabe von Eigenschaften

---
+++ src/components/StatusPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <h2>{{ dayOfWeek }}</h2>
+  <h3>Mittag</h3>
+  <div class="showpic">
+    <img
+      v-if="lunch"
+      :src="require('@/assets/greenhook.png')"
+      class="showpic"
+      alt="bestellt"
+    />
+    <img
+      v-else
+      :src="require('@/assets/redcross.png')"
+      class="showpic"
+      alt="nichtbestellt"
+    />
+  </div>
+  <h3>Vesper</h3>
+  <div class="showpic">
+    <img
+      v-if="snack"
+      :src="require('@/assets/greenhook.png')"
+      class="showpic"
+      alt="bestellt"
+    />
+    <img
+      v-else
+      :src="require('@/assets/redcross.png')"
+      class="showpic"
+      alt="nichtbestellt"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "StatusPage",
+  props: {
+    dayOfWeek: String,
+    snack: Boolean,
+    lunch: Boolean,
+  },
+});
+</script>
+
+<style scoped>
+.showpic {
+  height: 60px;
+  width: 60px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ src/views/StatusPageView.vue
@@ -0,0 +1,14 @@
+<template>
+  <status-page day-of-week="Dienstag" :lunch="true" :snack="true" />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import StatusPage from "@/components/StatusPage.vue";
+
+export default defineComponent({
+  name: "StatusPageView",
+  components: { StatusPage },
+});
+</script>
+
+<style scoped></style>
